cd00023d1454a6931dddfc26d5e7cf3ea2bdcb06
PengWang96 <pengwang1996@hotmail.com>
2024-10-24 22:55:16 +0800

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,63 @@
+#' @name dat.ha
+#' @alias dat.ha
+#' @docType data
+#' @title A Meta-Analysis on the Effect of Placebo Interventions for All Clinical
+#' Conditions Regarding Patient-Reported Outcomes
+#' @description This meta-analysis serves as an example to illustrate function usage in the package \pkg{altmeta}.
+#' @usage data("dat.ha")
+#' @format A data frame containing 109 studies with the observed effect sizes and their within-study variances.
+#' \describe{
+#'   \item{\code{y}}{the observed effect size for each collected study in the meta-analysis.}
+#'   \item{\code{s2}}{the within-study variance for each study.}
+#' }
+#' @source Hrobjartsson A, Gotzsche PC (2010). "Placebo interventions for all clinical conditions."
+#' \emph{Cochrane Database of Systematic Reviews}, \bold{1}. Art. No.: CD003974. <\doi{10.1002/14651858.CD003974.pub3}>
+#' @keyword dataset
+"dat.ha"
+
+#' @name dat.lcj
+#' @alias dat.lcj
+#' @docType data
+#' @title A Meta-Analysis on the Effect of Progressive Resistance Strength Training Exercise vs. Control
+#' @description This meta-analysis serves as an example to illustrate function usage in the package \pkg{altmeta}.
+#' @usage data("dat.lcj")
+#' @format A data frame containing 33 studies with the observed effect sizes and their within-study variances.
+#' \describe{
+#'   \item{\code{y}}{the observed effect size for each collected study in the meta-analysis.}
+#'   \item{\code{s2}}{the within-study variance for each study.}
+#' }
+#' @source Liu CJ, Latham NK (2009). "Progressive resistance strength training for
+#' improving physical function in older adults." \emph{Cochrane Database of Systematic Reviews},
+#' \bold{3}. Art. No.: CD002759. <\doi{10.1002/14651858.CD002759.pub2}>
+#' @keyword dataset
+"dat.lcj"
+
+#' @name dat.slf
+#' @alias dat.slf
+#' @docType data
+#' @title A Meta-Analysis on the Effect of Nicotine Gum for Smoking Cessation
+#' @description This meta-analysis serves as an example to illustrate function usage in the package \pkg{altmeta}.
+#' @usage data("dat.slf")
+#' @format A data frame containing 56 studies with the observed effect sizes and
+#' their within-study variances.
+#' \describe{
+#'   \item{\code{y}}{the observed effect size for each collected study in the meta-analysis.}
+#'   \item{\code{s2}}{the within-study variance for each study.}
+#' }
+#' @source Stead LF, Perera R, Bullen C, Mant D, Hartmann-Boyce J, Cahill K, Lancaster T (2012).
+#' "Nicotine replacement therapy for smoking cessation." \emph{Cochrane Database of Systematic Reviews},
+#' \bold{11}. Art. No.: CD000146. <\doi{10.1002/14651858.CD000146.pub4}>
+#' @keyword dataset
+"dat.slf"
+
+#' @name k_vec
+#' @alias k_vec
+#' @docType data
+#' @title Misclassification Probability and k-values for Levels of Heterogeneity
+#' @description This dataset contains a vector with 1000 values of \code{k},
+#' representing levels of heterogeneity, corresponding to 1000 sampled points of
+#' the misclassification probability $P_{\text{mis}}$ in the range of 0 to 0.05.
+#' @usage data("k_vec")
+#' @format A numeric vector of length 1000 containing k-values.
+#' @keyword dataset
+"k_vec"
---
+++ R/my_posterior.R
@@ -0,0 +1,563 @@
+#' Compute Posterior−PRP under distinguishability criterion using Metropolis-Hastings MCMC Algorithm
+#'
+#' Compute Posterior−PRP under distinguishability criterion. This function implements
+#' the Metropolis-Hastings algorithm for Markov Chain Monte Carlo simulations.
+#' @param N Total number of iterations in Metropolis-Hastings algorithm.
+#' @param r Burn-in ratio.
+#' @param m Number of replications for test statistics.
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @param test Type of test statistic to be used ("Q" or "Egger").
+#' @param side Type of hypothesis test ("one.side" or "two.side").
+#' @param heterogeneity_level Optional parameter to specify a tolerable heterogeneity level.
+#' @return A list containing the p-value, true effect estimates, and acceptance rate.
+#' @examples
+#' metropolis_hastings(10000, 0.05, 2, c(0.4, 0.6), c(0.1, 0.2))
+#' @export
+metropolis_hastings <- function(N, r, m, hat_beta, hat_sigma_sq, test = "Q",
+                                side = "one.side", heterogeneity_level = NULL) {
+  # Initialization
+  k_max <- if (!is.null(heterogeneity_level)) heterogeneity_level else 0.2726814
+  # k = 100, 0.5420572, 0.4626489, 0.3349839, 0.2726814 corresponding to P_mis = 0.5, 0.25, 0.2, 0.1, 0.05
+  bar_beta <- rep(hat_beta[1], N)
+  k <- rep(k_max, N)
+  count <- 0
+  count2 <- 0
+  acception <- 0
+  hat_beta_star_matrix <- matrix(nrow = m, ncol = N)  # to store all hat_beta_star values
+
+  # Main loop of the algorithm
+  for (i in 2:N) {
+    # # Sample bar_beta*
+    bar_beta_star <- rnorm(1,
+                           mean = mean(hat_beta),
+                           sd = sqrt(k_max^2 * mean(hat_beta)^2 + mean(hat_sigma_sq)))
+
+
+    # Sample k*
+    k_star <- if (!is.null(heterogeneity_level)) k_max else sample(k_vec, 1)
+
+    # Calculate acceptance ratio a
+    a1 <- g(bar_beta_star, hat_beta, hat_sigma_sq, k_star)
+    a2 <- q(bar_beta[i-1], hat_beta, hat_sigma_sq, k_max)
+    a3 <- g(bar_beta[i-1], hat_beta, hat_sigma_sq, k[i-1])
+    a4 <- q(bar_beta_star, hat_beta, hat_sigma_sq, k_max)
+    a <- exp(a1 + a2 - a3 - a4)
+    a <- min(1, a)
+
+    # Accept or reject
+    if (runif(1) < a) {
+      bar_beta[i] <- bar_beta_star
+      k[i] <- k_star
+      acception <- acception + 1
+    } else {
+      bar_beta[i] <- bar_beta[i-1]
+      k[i] <- k[i-1]
+    }
+
+    # Calculate test statistic and p-value
+    if (i > N * r) {
+      # varr <- 1 / (1 / (k[i]^2 * bar_beta[i]^2) + 1 / hat_sigma_sq)
+      # meann <- varr * (1 / (k[i]^2 * bar_beta[i]) + hat_beta / hat_sigma_sq)
+      if (k[i] != 0) {
+        varr <- (k[i]^2 * bar_beta[i]^2 * hat_sigma_sq) / (k[i]^2 * bar_beta[i]^2 + hat_sigma_sq)
+        meann <- varr * (k[i]^2 * bar_beta[i] * hat_beta + hat_sigma_sq) / (k[i]^2 * bar_beta[i] * hat_sigma_sq)##############
+        beta_star <- rnorm(m,
+                           mean = meann,
+                           sd = sqrt(varr))
+      } else {
+        beta_star <- bar_beta[i]
+      }
+
+
+      hat_beta_star <- rnorm(m, mean = beta_star, sd = sqrt(hat_sigma_sq))
+      hat_beta_star_matrix[, i] <- hat_beta_star  # store the hat_beta_star values
+
+      if (test == "Q") {
+        statistic_star <- T_Q(hat_beta_star, bar_beta[i], k[i], hat_sigma_sq)
+        statistic_ori <- T_Q(hat_beta, bar_beta[i], k[i], hat_sigma_sq)
+      } else if (test == "Egger") {
+        statistic_star <- egger(hat_beta_star, bar_beta[i], k[i], hat_sigma_sq, m)
+        statistic_ori <- egger(hat_beta, bar_beta[i], k[i], hat_sigma_sq, m)
+      }
+
+      if (test != "Hotelling") {
+        if (side == "one.side") {
+          if (statistic_star >= statistic_ori) {
+            count <- count + 1
+          }
+        } else {
+          if (statistic_star >= statistic_ori) {
+            count <- count + 1
+          } else {
+            count2 <- count2 + 1
+          }
+        }
+      }
+    }
+  }
+
+  if (test != "Hotelling") {
+    if (side == "one.side") {
+      p_value <- count / (N - N * r)
+    } else {
+      p_value <- 2 * min(count/(count + count2), count2/(count + count2))
+    }
+  } else {
+    beta_prime_H <- rowMeans(hat_beta_star_matrix[, (N*r+1):N])  # calculate the mean of hat_beta_star values
+    p_value <- hotelling_t_squared(beta_prime_H = beta_prime_H, hat_beta, hat_sigma_sq, df = N-N*r)
+  }
+
+  list(p_value = p_value, bar_beta = bar_beta, acception = acception/N)
+}
+
+
+
+
+#' Fit Fixed Effect Model using Monte Carlo Algorithm
+#'
+#' This function performs a fixed effect analysis.
+#' It calculates the p-value for the fixed effect model by comparing the test statistic
+#' of the observed data with that of the simulated data.
+#'
+#' @param N Total number of iterations for the Monte Carlo simulation.
+#' @param m Number of replications for the test statistic.
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#'
+#' @return A list containing the p-value, bar_beta estimates, and acceptance rate.
+#'
+#' @examples
+#' fixed_effect(10000, 0, 2, c(0.4, 0.6), c(0.1, 0.2))
+#'
+#' @export
+fixed_effect <- function(N, m, hat_beta, hat_sigma_sq) {
+  bar_beta <- rep(0, N)
+  k <- rep(0, N)
+  count <- 0
+
+  for (i in 1:N) {
+    varrr <- 1 / sum(1 / hat_sigma_sq)
+    meannn <- varrr * sum(hat_beta / hat_sigma_sq)
+    bar_beta[i] <- rnorm(1, mean = meannn, sd = sqrt(varrr))
+
+    hat_beta_star <- rnorm(m, mean = bar_beta[i], sd = sqrt(hat_sigma_sq))
+
+    T_Q_star <- T_Q(hat_beta_star, bar_beta[i], k[i], hat_sigma_sq)
+    T_Q_ori <- T_Q(hat_beta, bar_beta[i], k[i], hat_sigma_sq)
+
+    if (T_Q_star >= T_Q_ori) {
+      count <- count + 1
+      }
+  }
+
+  p_value <- count / N
+
+  list(p_value = p_value, bar_beta = bar_beta)
+}
+
+
+
+
+
+
+#' Calculate Posterior-PRP using numerical integration in fixed effect model
+#'
+#' This function calculates the Posterior-PRP (Posterior Predictive Replication Probability) value
+#' using numerical integration for a given set of observed effects and variances. It integrates over
+#' the posterior distribution of the true effect sizes `bar_beta` and computes the likelihood of
+#' observing `hat_beta_prime` under certain conditions.
+#'
+#' @param hat_beta A numeric vector representing observed effect sizes.
+#' @param hat_sigma_sq A numeric vector representing observed variances. It must have the same length as `hat_beta`.
+#' @param tol The maximum tolerance in integration, default 1e-2.
+#' @return A list containing:
+#'   \item{Integral_Value}{The computed Posterior-PRP value.}
+#'   \item{Integration_Error}{The estimated numerical integration error.}
+#' @examples
+#' # Example usage
+#' hat_beta <- c(1, 1)
+#' hat_sigma_sq <- c(0.1, 0.1)
+#' calc_PRP_FE_NumInt(hat_beta, hat_sigma_sq)
+#'
+#' @export
+calc_PRP_FE_NumInt <- function(hat_beta, hat_sigma_sq, tol = 1e-6) {
+  if (length(hat_sigma_sq) != length(hat_beta)) {
+    stop("The lengths of hat_beta and hat_sigma_sq must match.")
+  }
+
+  # Number of studies
+  m <- length(hat_beta)
+
+  # Calculate weights w_j = 1 / hat_sigma_j^2
+  w <- 1 / hat_sigma_sq
+
+  # Calculate posterior mean and variance for bar_beta
+  V_bar_beta <- 1 / sum(w)  # Variance
+  mu_bar_beta <- V_bar_beta * sum(w * hat_beta)  # Mean
+
+  # Constant c_0
+  c_0 <- sum(w * (hat_beta - mu_bar_beta)^2)
+
+  # Define the PDF for the Chi-square distribution with 1 df
+  d_chi2_1 <- function(x) {
+    ifelse(x >= 0, 1 / sqrt(2 * pi) * exp(-x / 2) / sqrt(x), 0)
+  }
+
+  # Define the CDF for the Chi-square distribution with m df
+  F_chi2_m <- function(x, m) {
+    pchisq(x, df = m)
+  }
+
+  # Define the integrand based on your theoretical derivation
+  integrand <- function(s) {
+    # Chi-squared CDF for m degrees of freedom
+    (1 - F_chi2_m(s + c_0, m)) * d_chi2_1(s)
+  }
+  # Numerical integration using integrate function
+  result <- integrate(integrand, lower = 0, upper = Inf, rel.tol = tol)
+  # Return the results
+  list(Integral_Value = result$value, Integration_Error = result$abs.error)
+}
+
+
+
+
+
+
+#' Calculate the weighted mean of beta estimates
+#'
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @return A numeric value representing the weighted mean of the effect estimates.
+#' @examples
+#' hat_beta <- c(0.5, 1.0, 1.5)
+#' hat_sigma_sq <- c(0.2, 0.3, 0.4)
+#' mean_Q(hat_beta, hat_sigma_sq)
+mean_Q <- function(hat_beta, hat_sigma_sq) {
+  w <- 1 / hat_sigma_sq
+  return(sum(w * hat_beta) / sum(w))
+}
+
+#' Calculate the p-value for Cochran's Q test
+#'
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @return A numeric value representing the p-value for Cochran's Q test.
+#' @examples
+#' hat_beta <- c(0.5, 1.0, 1.5)
+#' hat_sigma_sq <- c(0.2, 0.3, 0.4)
+#' frequency_pvalue(hat_beta, hat_sigma_sq)
+frequency_pvalue <- function(hat_beta, hat_sigma_sq) {
+  statistic <- T_Q(hat_beta, mean_Q(hat_beta, hat_sigma_sq), 0, hat_sigma_sq)
+  p_value <- 1 - pchisq(statistic, df = length(hat_beta) - 1)
+  return(p_value)
+}
+
+
+
+
+#' Probability of Misclassification Function
+#'
+#' This function calculates the probability of misclassification for a given value of k.
+#' @param k A numeric value representing the level of heterogeneity.
+#' @return Numeric value of the probability of misclassification.
+#' @examples
+#' P_mis(1)
+#' @export
+P_mis <- function(k) {
+  integrand <- function(z) {
+    1 / (1 + exp(0.5 * (1/k)^2 + z/k)) * dnorm(z)
+  }
+  result <- integrate(integrand, lower = -Inf, upper = Inf)
+  return(result$value)
+}
+
+
+
+
+#' Inverse Probability of Misclassification Function
+#'
+#' This function approximates the inverse function value of P_{mis}(k) using the bisection method.
+#' @param P_mis_val The value of P_{mis} for which the inverse is sought.
+#' @param lower The lower bound for the bisection method.
+#' @param upper The upper bound for the bisection method.
+#' @param tol The tolerance level for the bisection method.
+#' @param max_iter The maximum number of iterations for the bisection method.
+#' @return Numeric value of the approximate inverse function value of P_{mis}.
+#' @examples
+#' inverse_P_mis(0.05)
+#' @export
+inverse_P_mis <- function(P_mis_val, lower = 1e-6, upper = 100,
+                          tol = 1e-6, max_iter = 1000) {
+  root_func <- function(k) P_mis(k) - P_mis_val
+  iter <- 0
+  while (upper - lower > tol && iter < max_iter) {
+    mid <- (lower + upper) / 2
+    if (root_func(mid) * root_func(lower) <= 0) {
+      upper <- mid
+    } else {
+      lower <- mid
+    }
+    iter <- iter + 1
+  }
+  return((lower + upper) / 2)
+}
+
+
+
+
+
+
+
+#' Function g by log transformation
+#'
+#' This function calculates the log-transformed value of g.
+#' @param bar_beta The estimated true underlying effect.
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @param k The level of heterogeneity.
+#' @return Numeric value of the log-transformed function g.
+#' @examples
+#' g(0.5, c(0.4, 0.6), c(0.1, 0.2), 1)
+#' @export
+g <- function(bar_beta, hat_beta, hat_sigma_sq, k) {
+  m <- length(hat_beta)  # Number of estimated effects
+  sum_log_terms <- 0  # Initialize sum of log terms
+
+  for (j in 1:m) {
+    sigma_j_sq <- hat_sigma_sq[j]
+    beta_j <- hat_beta[j]
+
+    # Calculate the variance term
+    variance_term <- k^2 * bar_beta^2 + sigma_j_sq
+
+    # Log of the first part of the product (normalization factor)
+    log_normalization <- -0.5 * log(variance_term)
+
+    # Log of the second part (exponent term)
+    log_exponent <- -0.5 * ((beta_j - bar_beta)^2 / variance_term)
+
+    # Add these log terms
+    sum_log_terms <- sum_log_terms + log_normalization + log_exponent
+  }
+  return(sum_log_terms)
+}
+
+
+
+
+
+#' Proposed Function q by log transformation
+#'
+#' This function calculates logarithm of the proposed density used in Metropolis-Hastings (MH).
+#' @param bar_beta_star The simulated value of bar_beta.
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @param k The level of heterogeneity.
+#' @return Logarithm of the density of the normal distribution for bar_beta_star.
+#' @examples
+#' q(0.5, c(0.4, 0.6), c(0.1, 0.2), 1)
+#' @export
+q <- function(bar_beta_star, hat_beta, hat_sigma_sq, k) {
+  k_max <- k
+  res <- dnorm(bar_beta_star,
+               mean = mean(hat_beta),
+               sd = sqrt(k_max^2 * mean(hat_beta)^2 + mean(hat_sigma_sq)), log = T)
+  return(res)
+}
+
+
+# q <- function(bar_beta_star, hat_beta, hat_sigma_sq, k) {
+#   varrr <- 1 / sum(1 / hat_sigma_sq)
+#   meannn <- varrr * sum(hat_beta / hat_sigma_sq)
+#   res <- dnorm(bar_beta_star,
+#                mean = meannn,
+#                sd = sqrt(varrr), log = T)
+#   return(res)
+# }
+
+
+
+
+
+
+
+#' Modified Cochran’s Q statistic T_Q
+#'
+#' This function calculates the modified Cochran’s Q statistic.
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param bar_beta The estimated true underlying effect.
+#' @param k The level of heterogeneity.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @return Numeric value of the test statistic T_Q.
+#' @examples
+#' T_Q(c(0.4, 0.6), 0.5, 1, c(0.1, 0.2))
+#' @export
+T_Q <- function(hat_beta, bar_beta, k, hat_sigma_sq) {
+  w <- 1 / (hat_sigma_sq + k^2 * bar_beta^2)
+  sum(w * (hat_beta - bar_beta)^2)
+}
+
+
+
+
+#' Modified Egger test statistics
+#'
+#' This function provides the calculation for Egger test quantities.
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @param bar_beta The estimated true underlying effect.
+#' @param k The level of heterogeneity.
+#' @param m The number of replications.
+#' @return The Egger test statistic value.
+#' @examples
+#' egger(c(0.4, 0.6), 0.5, 1, c(0.1, 0.2), 2)
+#' @export
+egger <- function(hat_beta, bar_beta, k, hat_sigma_sq, m){
+  y <- hat_beta/sqrt(hat_sigma_sq + k^2*bar_beta^2)
+  x <- 1/sqrt(hat_sigma_sq + k^2*bar_beta^2)
+
+  Sxx <- sum( (x-mean(x))*(x-mean(x)) )
+  Sxy <- sum( (x-mean(x))*(y-mean(y)) )
+  Syy <- sum( (y-mean(y))*(y-mean(y)) )
+
+  b1 <- Sxy/Sxx
+  b0 <- mean(y)- b1*mean(x)
+
+  s2 <- (Syy - b1^2*Sxx)/ (m - 2)
+  vb0 <- s2 * (1 / m + mean(x)^2 / Sxx)
+
+  egger_test <- b0^2/vb0
+  return(egger_test)
+}
+
+
+
+
+#' Hotelling T-squared Statistic Function
+#'
+#' This function calculates the Hotelling T-squared statistic for multivariate hypothesis testing.
+#' @param beta_prime_H The mean of estimated true underlying effect.
+#' @param hat_beta The original or the simulated estimated effects.
+#' @param hat_sigma_sq The squared standard errors of the estimated effects.
+#' @param df Degrees of freedom for the test.
+#' @return p-value for the Hotelling T-squared test.
+#' @examples
+#' hotelling_t_squared(c(0.4, 0.6), c(0.5, 0.7), c(0.1, 0.2), 9500)
+#' @export
+hotelling_t_squared <- function(beta_prime_H, hat_beta, hat_sigma_sq, df) {
+  S <- diag(hat_sigma_sq)
+  m <- length(hat_beta)
+  T_H <- df * t(beta_prime_H - hat_beta) %*% solve(S) %*% (beta_prime_H - hat_beta)
+  F_value <- (df - m) / ((df - 1) * m) * T_H
+  p_value <- 1 - pf(F_value, m, df - m)
+  return(p_value)
+}
+
+
+
+
+#' Combine Results Function
+#'
+#' This function combines the results from multiple Metropolis-Hastings simulations.
+#' @param ... Variable number of Metropolis-Hastings result lists.
+#' @return A list containing combined p-values, bar_beta estimates, and average acceptance rate.
+#' @examples
+#' combine_results(result1, result2, result3)
+#' @export
+combine_results <- function(...) {
+  args <- list(...)
+
+  combined_p_value <- c()
+  combined_bar_beta <- NULL
+  combined_acception <- 0
+
+  for (arg in args) {
+    combined_p_value <- c(combined_p_value, arg$p_value)
+    combined_acception <- combined_acception + arg$acception
+
+    if (is.null(combined_bar_beta)) {
+      combined_bar_beta <- arg$bar_beta
+    } else {
+      combined_bar_beta <- rbind(combined_bar_beta, arg$bar_beta)
+    }
+  }
+
+  total_acception <- combined_acception / length(args)
+
+  list(p_value = combined_p_value,
+       bar_beta = combined_bar_beta,
+       acception = total_acception)
+}
+
+
+
+
+#' Shuffle Function
+#'
+#' This function randomly shuffles elements in a vector.
+#' @param xv Input vector to be shuffled.
+#' @param rep Number of times to perform the shuffle.
+#' @return Shuffled vector.
+#' @examples
+#' shuffle(c(1, 2, 3, 4, 5), 2)
+#' @export
+shuffle <- function(xv, rep){
+  yv = xv
+  for (i in 1:rep){
+    index = sample(1:length(xv),2, replace=F)
+    temp = yv[index[1]]
+    yv[index[1]] = yv[index[2]]
+    yv[index[2]] = temp
+  }
+  return(yv)
+}
+
+
+
+
+#' Simulate Batch Contamination Function
+#'
+#' This function simulates batch contamination in a dataset.
+#' @param bb_sd Standard deviation for batch effects.
+#' @param k Level of heterogeneity.
+#' @param bbar True underlying effect.
+#' @return Vector containing the simulated data.
+#' @examples
+#' sim_batch(0.1, 1, 0.5)
+#' @export
+sim_batch<-function(bb_sd, k, bbar){
+  grp_num <- m
+
+  btv = bbar + rnorm(grp_num, sd = sqrt(k^2*bbar^2))
+
+  bbv = rep(0, grp_num)
+  bbv[1:bc_grp_num]=rnorm(bc_grp_num, sd = bb_sd)
+
+  rst = c()
+  for (i in 1:grp_num){
+    gv = Gv[i,]
+    batch = Batch[i,]
+    y = btv[i]*gv + bbv[i]*batch + rnorm(sample_size, sd=rnse)
+    m = summary(lm(y~gv))
+    rst = c(rst, m$coef[2,1], m$coef[2,2])
+  }
+
+  return(rst)
+}
+
+
+
+
+#' Censoring Function
+#'
+#' This function applies a censoring mechanism based on a power law.
+#' @param p Numeric vector of probabilities to be censored.
+#' @return Censored probabilities.
+#' @examples
+#' wipi(0.1)
+#' @export
+wipi<-function(p){
+  return(exp(-c*p^(1.5)))
+}
